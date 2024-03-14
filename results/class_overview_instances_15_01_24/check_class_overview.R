ae1ee42ae81b3e87569434d626667103ad3cd94f
Jannik <vikari.jannik@gmail.com>
2024-03-14 11:03:40 +0100

    changed the class names in the txt

--- results/class_overview_instances_15_01_24/check_class_overview.R
+++ results/class_overview_instances_15_01_24/check_class_overview.R
@@ -25,6 +25,9 @@ overview_valid <- read.table("cls_overview_valid.txt",
 all_data <- merge(classes_overview, overview_train, by = "class") %>% 
   merge(., overview_valid, by = "class") %>% 
   merge(., overview_test, by = "class") %>%
-  mutate(sum_count = count_train + count_valid + count_test)
+  mutate(sum_count = count_train + count_valid + count_test) %>% 
+  arrange(., desc(sum_count)) %>% 
+  mutate(class_name = str_replace_all(class_name, "_", " ")) %>%
+  mutate(class_name = str_to_title(class_name))
 
 write.table(all_data, "combined_overview.txt", quote = FALSE, sep = "\t", row.names = FALSE)  
