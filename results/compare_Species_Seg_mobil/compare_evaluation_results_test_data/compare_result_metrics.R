4407ed6e7f0481b35049cc7bf6d48a77e340d60d
Jannik <vikari.jannik@gmail.com>
2024-03-22 12:44:40 +0100

    changed the graphic for model comparison

--- results/compare_Species_Seg_mobil/compare_evaluation_results_test_data/compare_result_metrics.R
+++ results/compare_Species_Seg_mobil/compare_evaluation_results_test_data/compare_result_metrics.R
@@ -1,12 +1,8 @@
 library(tidyverse)
+library(latex2exp)
 # Install and load the package
-library("ggsci")
 
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-
-wf = windowsFonts()
-
-names(wf[wf=="TT Times New Roman"])
 
 lowercase_second_word <- function(x) {
   s <- strsplit(x, " ")[[1]]
@@ -14,20 +10,14 @@ lowercase_second_word <- function(x) {
   return(paste(s, collapse = " "))
 }
 
-df_metrics_base_n <- read.table("metrics_yolo_n_base_conf_standard.txt", header = T)
+df_metrics_base_n <- read.table("plots_and_tables/metrics_yolo_n_base_conf_standard.txt", header = T)
   
-
-df_metrics_weighted_aug_n <- read.table("metrics_yolo_n_weighted_sampler_augment_conf_standard.txt", header = T)
-
-df_metrics_weighted_n <- read.table("metrics_yolo_n_weighted_sampler_conf_standard.txt", header = T)
-
+df_my_model <- read.table("plots_and_tables/metrics_yolo_n_weighted_sampler_augment_conf_standard.txt", header = T)
 # combine data
 data <- df_metrics_base_n %>% 
-  merge(., df_metrics_weighted_n, by=c("species", "class_index")) %>% 
-  merge(., df_metrics_weighted_aug_n, by=c("species", "class_index"))
-
-data_plot <- data %>% 
-  select(species, map_50_95_mask_n_base, map_50_95_mask_n_weighted, map_50_95_mask_n_weighted_augment) %>% 
+  merge(., df_my_model, by=c("species", "class_index")) %>% 
+  select(class_index, species, f1_mask_n_base, f1_mask_n_weighted_augment,
+         map_50_95_mask_n_base, map_50_95_mask_n_weighted_augment) %>% 
   mutate(species = replace(species, str_detect(species, "all"), "aall")) %>%
   arrange(species) %>%
   mutate(species = replace(species, str_detect(species, "aall"), "all")) %>% 
@@ -35,24 +25,28 @@ data_plot <- data %>%
   mutate(species = str_to_title(species))
 
 # Apply the function to your column
-data_plot$species <- sapply(data_plot$species, lowercase_second_word)
-data_plot$species[1] <- "All"
+data$species <- sapply(data$species, lowercase_second_word)
+data$species[1] <- "All"
+
+
+data_plot <- data %>% 
+  select(species, map_50_95_mask_n_base,  map_50_95_mask_n_weighted_augment) 
+  
 
 write.table(data_plot, "combined_metrics.txt", sep = "\t", row.names = FALSE, quote=FALSE)
 
 
 # compare model MAP 50 95 mask between models:
 data_plot %>%
-  rename("BM" = map_50_95_mask_n_base,
-         "WSM" = map_50_95_mask_n_weighted,
-         "WSAM" = map_50_95_mask_n_weighted_augment) %>% 
+  rename("YOLOv8n-seg" = map_50_95_mask_n_base,
+         "SpeciesSeg-mobil" = map_50_95_mask_n_weighted_augment) %>% 
   pivot_longer(!species, names_to = "Model", values_to = "map_05_095") %>% 
   ggplot(., aes(x=fct_inorder(species), y = map_05_095, fill = Model)) + 
   geom_bar(stat='identity', position = position_dodge()) +
   scale_fill_manual(values = cbPalette[2:6]) +
   coord_cartesian(ylim = c(0, 1)) +
-  scale_x_discrete("Species") + 
-  scale_y_continuous("MAP (50-95)", breaks = seq(0, 1, by = 0.2)) +
+  scale_x_discrete("Tree species") + 
+  scale_y_continuous(TeX("Segment mAP $_{@0.5-0.95}$"), breaks = seq(0, 1, by = 0.2)) +
   # ggtitle("Comparison of MAP (50-95) for masks between YOLO models") +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
